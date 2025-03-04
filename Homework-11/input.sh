d4171485341873e547f85f731d91b359483289b4
EC2 Default User <ec2-user@ip-172-31-92-138.ec2.internal>
2025-03-04 06:18:10 +0000

    homework-11

---
+++ Homework-11/input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name=Samara
+age=25
+favorite_language=Java
+hobby=Volleyball 
+dream="to be succesful"
+
+echo "Hi,my name is $name, and I'm $age years old. Every morning, $name likes to spend time &hobby.
+As a $age-year-old, I belive it's never too late to follow your dreams. My favotite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream.
+when I'm not busy working toward $dream, you'll find me $hobby to relax.
+That's a little about me, $name - a $age-year-old who loves $favorite_language, enjoys $hobby, enjoys $hobby, and dreams of $dream!"
---
+++ Homework-11/introduce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Hi, what is your name? " name
+read -p "How old are you? " age
+read -p "What is your favorite language? " favorite_language
+read -p "What is your hobby? " hobby
+read -p "What is your dream? " dream
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby. 
+As a $age-years-old, I belive it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achive my dream of $dream.
+When I'm not busy working toward $dream, you'll find me $hobby to relax.
+That's a little about me, $name - a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
